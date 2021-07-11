d47bf8ca1eb4eff3b3a46635443f1f980e3261a2
Chandra Dubey <chandraprak33@gmail.com>
2021-07-12 04:14:15 +0530

    new job api created

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -1,12 +1,33 @@
 const pool = require('../config/pg');
 
-module.exports ={
-    alljobs : (req, res)=>{
-         pool.query('SELECT * FROM jobs ORDER BY created_at DESC')
-         .then(result=> res.json({status:200, data:result.rows}))
-         .catch(err=>{
-             res.json({status:500, message:err});
-         })
+module.exports = {
+    allJobs: (req, res) => {
+        try{
+            pool.query('SELECT * FROM jobs ORDER BY created_at DESC').then(result => res.json({status: 200, data: result.rows})).catch(err => {
+                res.json({status: 500, message: err});
+            })
+        }
+        catch(err){
+            res.json({status: 500, message: err});
+        }
+       
     },
-    
+    newJob: (req, res) => {
+        try{
+            const {
+                title,
+                description,
+                company_name,
+                city,
+                recruiter_id
+            } = req.body;
+            const result = pool.query("INSERT INTO jobs (title, description, company_name, city, recruiter_id) VALUES ($1, $2 ,$3, $4, $5) RETURNING*",
+            [title, description, company_name, city, recruiter_id])
+            .then(()=> res.json({status:200, message:"new job added successfully"}))
+        }
+        catch (err){
+            res.json({status: 500, message: err});
+        }
+    }
+
 }
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const jobController = require('../controllers/jobController');
 const passport = require('passport');
 
-router.get('/all', jobController.alljobs);
+router.get('/all', jobController.allJobs);
+router.post('/new', jobController.newJob);
 
 module.exports = router;
